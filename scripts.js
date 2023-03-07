0381c0e805932e7eacd0618b63d09a726e1d3bdd
Mariia <shvetsmariia@yahoo.com>
2023-03-07 12:18:56 -0600

    modified validation functions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,36 @@
 document.getElementById("registerButton").addEventListener("click", function(event){
     event.preventDefault()
 });
+
+const inputName = document.getElementById('name');
+const inputPhone = document.getElementById('phone');
+const inputEmail = document.getElementById('email');
+const inputPassword = document.getElementById('password');
+
+function formValidation() {
+
+    if (validateName(inputName) == true) {
+    } else 
+    {
+        alert("Enter a valid name"); 
+    }
+
+    if (validatePhone(inputPhone) == true) {
+    } else 
+    {
+        alert("Enter a valid phone number"); 
+    }
+
+    if (validateEmail(inputEmail) == true) {
+    } else 
+    {
+        alert("Enter a valid email"); 
+
+    }
+    
+    if (validatePassword(inputPassword) == true) {
+    } else 
+    {
+        alert("Enter a valid password");
+    }
+}
